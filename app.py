5d7496e2ba875583161087a7ed57d0a1f3b2309f
vishal <vishalhirani2007@gmail.com>
2026-03-08 11:33:57 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from pytubefix import YouTube
-import os
 
 st.title(" YT Downloader")
 
@@ -19,24 +18,15 @@ if st.button("Download"):
             else:
                 stream = yt.streams.filter(only_audio=True).first()
             
-            
             file_path = stream.download(output_path=".")
-            
             
             with open(file_path, "rb") as f:
                 st.download_button(
-                    label="📥 Click here to Save File",
+                    label=" Download Now",
                     data=f,
-                    file_name=os.path.basename(file_path),
+                    file_name="downloaded_file.mp4" if option == "Video" else "audio.mp3",
                     mime="video/mp4" if option == "Video" else "audio/mpeg"
                 )
-            st.success("Download Ready!")
-            
-            
-            
+            st.success(" Download Ready!")
         except Exception as e:
             st.error(f"Error: {e}")
-    else:
-
-        st.warning("Please enter a URL first!")
-
